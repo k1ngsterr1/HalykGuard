cbdf24d646afd3619807533e4b5a4b9239e868ed
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-01-27 19:09:21 +0600

    Version 1.0.7

--- screens/Registration/index.tsx
+++ screens/Registration/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-svg";
+import { globalStyles } from "shared/styles/global";
 
 export const RegistrationScreen = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+  return <View style={globalStyles.container}></View>;
 };
