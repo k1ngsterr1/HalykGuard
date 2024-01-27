ff171d5a347edcf37ea299f927458ae0c007ddf1
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-01-27 16:26:38 +0600

    Version 1.0.4

--- app/App.tsx
+++ app/App.tsx
@@ -3,10 +3,26 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { store } from "redux/store";
-
+import { useFonts } from "expo-font";
+import { Fonts } from "shared/styles/theme";
 import AppStack from "screens";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    [Fonts.regular]: require("assets/fonts/OpenSans/OpenSans-Regular.ttf"),
+    [Fonts.medium]: require("assets/fonts/OpenSans/OpenSans-Medium.ttf"),
+    [Fonts.semiBold]: require("assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
+    [Fonts.bold]: require("assets/fonts/OpenSans/OpenSans-Bold.ttf"),
+    [Fonts.extraBold]: require("assets/fonts/OpenSans/OpenSans-ExtraBold.ttf"),
+
+    // Italic
+    [Fonts.lightItalic]: require("assets/fonts/OpenSans/OpenSans-LightItalic.ttf"),
+    [Fonts.italic]: require("assets/fonts/OpenSans/OpenSans-Italic.ttf"),
+    [Fonts.italicMedium]: require("assets/fonts/OpenSans/OpenSans-MediumItalic.ttf"),
+    [Fonts.italicSemiBold]: require("assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
+    [Fonts.italicBold]: require("assets/fonts/OpenSans/OpenSans-Bold.ttf"),
+  });
+
   return (
     <Provider store={store}>
       <View style={{ flex: 1 }}>
--- shared/styles/theme.ts
+++ shared/styles/theme.ts
@@ -8,7 +8,18 @@ export const Colors = {
 };
 
 export const Fonts = {
+  // Regular Fonts
+  light: require("assets/fonts/OpenSans/OpenSans-Light.ttf"),
   regular: require("assets/fonts/OpenSans/OpenSans-Regular.ttf"),
-  // regular: require("assets/fonts/OpenSans/OpenSans-Regular.ttf"),
-  bold: "OpenSans-Bold",
+  medium: require("assets/fonts/OpenSans/OpenSans-Medium.ttf"),
+  semiBold: require("assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
+  bold: require("assets/fonts/OpenSans/OpenSans-Bold.ttf"),
+  extraBold: require("assets/fonts/OpenSans/OpenSans-ExtraBold.ttf"),
+
+  // Italic Fonts
+  lightItalic: require("assets/fonts/OpenSans/OpenSans-LightItalic.ttf"),
+  italic: require("assets/fonts/OpenSans/OpenSans-Italic.ttf"),
+  italicMedium: require("assets/fonts/OpenSans/OpenSans-MediumItalic.ttf"),
+  italicSemiBold: require("assets/fonts/OpenSans/OpenSans-SemiBold.ttf"),
+  italicBold: require("assets/fonts/OpenSans/OpenSans-Bold.ttf"),
 };
