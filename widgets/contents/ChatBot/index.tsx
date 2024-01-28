07b2156119d992a17bc2b0c42b86d02009e4b599
Chechen Daddy <ruslanmakhmatov@gmail.com>
2024-01-28 06:40:21 +0600

    Version 1.2.6

--- widgets/contents/ChatBot/index.tsx
+++ widgets/contents/ChatBot/index.tsx
@@ -5,12 +5,13 @@ import { globalStyles } from "shared/styles/global";
 import { styles } from "./styles";
 import { UpperTabBack } from "features/UpperTabs/ui";
 import { ChatUnderTab } from "features/ChatUnderTab/ui";
+import ChatScreen from "widgets/ChatScreen";
 
 export const ChatBotContent: React.FC = () => {
   return (
     <SafeAreaView style={globalStyles.container}>
       <UpperTabBack headingTitle="Чат Бот" backNavigation="Home" />
-      <ChatUnderTab />
+      <ChatScreen />
     </SafeAreaView>
   );
 };
